212d1ff9cd9fafc933fa4dba9a0c0a6a05733af4
TanaKeKe <phanthanhtan2004@gmail.com>
2024-11-14 23:06:29 +0700

    feat: UI game play

--- Assets/Scripts/GamePlay/Panel/GamePlayPanel.cs
+++ Assets/Scripts/GamePlay/Panel/GamePlayPanel.cs
@@ -1,10 +1,59 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class GamePlayPanel : MonoBehaviour
+public class GamePlayPanel : Panel
 {
+    [SerializeField] private TextMeshProUGUI levelMaxText;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI scoreBestText;
+    [SerializeField] private TextMeshProUGUI livesText;
+    [SerializeField] private Image countTimeImage;
+    [SerializeField] private float fillTime;
+
+    
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(countTimeImage.fillAmount > 0)
+        {
+            Debug.Log(countTimeImage.fillAmount);
+            countTimeImage.fillAmount -= Time.deltaTime * fillTime;
+        }
+    }
+
     public void Replay()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void BackHome()
+    {
+
+    }
+
+    public void TurnOnAchievement()
+    {
+
+    }
+
+    public void Pause()
+    {
+
+    }
+
+    public void ShowTutorial()
+    {
+
     }
 }
